b61bd3fc30786cfa074f1fcb5add2101b617ace6
Ashfid <ashfidable.islam@gmail.com>
2023-04-01 04:39:11 +0300

    chore: deploy actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ashfidable.github.io',
+  base: '/pokemon-astro',
   integrations: [tailwind()]
 });
