32731e60855f260d79431545b5c3e81ece164fa5
Ashfid <ashfidable.islam@gmail.com>
2023-04-02 20:35:26 +0300

    chore: dependencies changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ashfidable.github.io',
   base: '/pokemon-astro',
-  integrations: [tailwind()]
+  integrations: [tailwind(), svelte(), image()]
 });
