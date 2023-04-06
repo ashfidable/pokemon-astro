da7233f289e8cfddd5634a212a0b8c0f807190e6
Ashfid <ashfidable.islam@gmail.com>
2023-04-07 01:50:34 +0300

    feat: added stats field for pokemon types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -7,6 +7,13 @@ export const PokemonSchema = z.object({
         front_default: z.string(),
         other: z.any()
     }),
+    stats: z.object({
+        base_stat: z.number(),
+        effort: z.number(),
+        stat: z.object({
+            name: z.string(),
+        })
+    }).array(),
     types: z.object({
         type: z.object({
             name: z.string()
