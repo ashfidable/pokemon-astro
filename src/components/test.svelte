585b082683abe4073742838ca0b2a6470b80da2e
Ashfid <ashfidable.islam@gmail.com>
2023-04-07 01:56:31 +0300

    chore: deleted test component

--- src/components/test.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import { getAllPokemons } from "../lib/pokemon";
-    import Pagination from "./pagination.svelte";
-
-    export let pokemons;
-</script>
-
-<Pagination items={pokemons} />
